d395424aa6209d7375cc173b5ed26edbe8f7c28e
Asad Ismail <asadismaeel@gmail.com>
2023-04-02 13:09:07 +0200

    updated front end

--- frontend/pixel-vision/src/app/app.component.ts
+++ frontend/pixel-vision/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  startProject() {
+    // Add functionality to start a new project here
+  }
   title = 'PixelVisionX';
+  
 }
