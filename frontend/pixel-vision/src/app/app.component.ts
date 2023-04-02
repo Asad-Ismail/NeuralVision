b326c3a504977a0cd45aa04e86780a382bc55527
Asad Ismail <asadismaeel@gmail.com>
2023-04-02 13:51:48 +0200

    updated neural csss

--- frontend/pixel-vision/src/app/app.component.ts
+++ frontend/pixel-vision/src/app/app.component.ts
@@ -3,12 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css','./neural.css'],
 })
 export class AppComponent {
   startProject() {
     // Add functionality to start a new project here
   }
   title = 'PixelVisionX';
-  
 }
