1595e960faad82eb4b695df4bfa345fc10b81533
Asad-Ismail <asadismaeel@gmail.com>
2023-04-24 23:15:40 +0200

    updated visualiyation

--- backend/vis_maskrcnn_dataset.py
+++ backend/vis_maskrcnn_dataset.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from maskrcnn import *
+import cv2
 
 def visualize_dataset(dataset, idx):
     # Get the image and target from the dataset
@@ -14,16 +15,17 @@ def visualize_dataset(dataset, idx):
     # Create a figure and axis to plot the image
     fig, ax = plt.subplots(1, figsize=(12, 12))
     ax.imshow(image_np)
-
+    idx=0
     # Iterate over the bounding boxes and masks
     for bbox, mask in zip(target["boxes"], target["masks"]):
         x, y, w, h = bbox.numpy()
         rect = patches.Rectangle((x, y), w, h, linewidth=1, edgecolor='r', facecolor='none')
         ax.add_patch(rect)
-
         # Overlay the mask on the image
+        cv2.imwrite(f"mask_{idx}.png", mask.numpy().astype(np.uint8) * 255)
         mask_np = mask.numpy().astype(np.float32)
         ax.imshow(np.ma.masked_array(mask_np, mask_np == 0), alpha=0.4, cmap="jet")
+        idx+=1
 
     plt.show()
 
